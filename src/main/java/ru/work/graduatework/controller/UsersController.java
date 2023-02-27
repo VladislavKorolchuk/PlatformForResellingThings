7474d42690b6fe3aa8e03165fee766b67a4ae3ba
Анастасия <fumi_nako@mail.ru>
2023-02-28 01:10:28 +0300

    Документация контроллеров пользователей

--- src/main/java/ru/work/graduatework/controller/UsersController.java
+++ src/main/java/ru/work/graduatework/controller/UsersController.java
@@ -1,40 +1,129 @@
 package ru.work.graduatework.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.work.graduatework.Entity.Users;
 import ru.work.graduatework.service.UsersService;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
 public class UsersController {
     private final UsersService usersService;
-    public ResponseEntity<?> getAds() {
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
-    @GetMapping("/me")   // Получить пользователя
-    public ResponseEntity<?> getUsers() {
-        return ResponseEntity.status(HttpStatus.OK).build();
+
+    @Operation(summary = "Получить пользователя",
+            operationId = "getUser_1",
+            responses = {@ApiResponse
+                    (responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.ALL_VALUE,
+                                    schema = @Schema(implementation = Users.class)
+                            )),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found"
+                    )
+            }, tags = "USER"
+    )
+    @GetMapping("/users/me")
+    public String getUsers() {
+        return usersService.getUsers();
     }
 
-    @PostMapping("/set_password")  // Установка пароля
-    public ResponseEntity<?> setPassword() {
-        return ResponseEntity.status(HttpStatus.OK).build();
+    @Operation(summary = "Установить пароль",
+            operationId = "setPassword",
+            responses = {@ApiResponse
+                    (responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Users.class) // пользователи - нужен пароль
+                            )),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized"
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found"
+                    )
+            }, tags = "USER"
+    )
+    @PostMapping("/users/set_password")
+    public String setPassword() {
+        return usersService.setPassword();
     }
 
-    @PatchMapping("/me")  // Обновить пользователя
-    public ResponseEntity<?> updateUser() {
-        return ResponseEntity.status(HttpStatus.OK).build();
+    @Operation(summary = "Обновить данные пользователя",
+            operationId = "updateUser",
+            responses = {@ApiResponse
+                    (responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.ALL_VALUE,
+                                    schema = @Schema(implementation = Users.class)
+                            )),
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Unauthorized"
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized"
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Forbidden"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found"
+                    )
+            }, tags = "USER"
+    )
+    @PatchMapping("/users/me")
+    public String updateUser() {               //   required: true
+        return usersService.updateUser();
     }
 
-    @PatchMapping("/me/image") // Обновление изображение пользователя
-    public ResponseEntity<?> updateUserImage() {
-        return ResponseEntity.status(HttpStatus.OK).build();
+    @Operation(summary = "Обновление изображение пользователя",
+            responses = {@ApiResponse
+                    (responseCode = "200",
+                            description = "OK",
+                            content = @Content(
+                                    mediaType = MediaType.MULTIPART_FORM_DATA_VALUE
+                            )),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Not Found"
+                    )
+            }, tags = "USER"
+    )
+    @PatchMapping("/users/me/image")
+    public String updateUserImage() {
+        return usersService.updateUserImage();
     }
-
-
 }
