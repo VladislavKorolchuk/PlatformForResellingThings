513e4cf2c934fa640dca8f9939209a4e03973a5f
93294 <9329496@mail.ru>
2023-02-27 15:49:20 +0300

    Initial commit

---
+++ src/test/java/ru/work/graduatework/GraduateWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.work.graduatework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduateWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
