1cf2314a6c895b9736c0f6d156225a15ae6f1b36
PDemenkov <guapnik@gmail.com>
2023-03-06 22:34:11 +0300

    Image controller и UsersController сигнатуры методов и параметры

--- src/main/java/ru/work/graduatework/controller/ImageController.java
+++ src/main/java/ru/work/graduatework/controller/ImageController.java
@@ -10,9 +10,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController()
 @RequiredArgsConstructor
@@ -26,8 +25,9 @@ public class ImageController {
                     @ApiResponse(responseCode = "404",
                             description = "Not Found")}, tags = "Изображения")
     @PostMapping("/{id}")
-    public ResponseEntity<?> updateAdsImage() {
+    public ResponseEntity<?> updateAdsImage(@PathVariable int id, @RequestParam MultipartFile imageFile) {
         logger.info("Current Method is - updateAdsImage");
+
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
