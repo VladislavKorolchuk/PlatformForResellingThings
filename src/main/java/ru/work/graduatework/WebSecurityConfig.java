3d1068e003ae2bfe1477fdf646b145fb4b3c7970
PDemenkov <guapnik@gmail.com>
2023-03-21 00:43:37 +0300

    deleted

--- src/main/java/ru/work/graduatework/WebSecurityConfig.java
+++ src/main/java/ru/work/graduatework/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,7 +23,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableMethodSecurity
 @Configuration
-public class WebSecurityConfig {
+public class WebSecurityConfig   {
     private static final String[] AUTH_WHITELIST = {
             "/swagger-resources/**",
             "/swagger-ui.html",
@@ -31,36 +32,12 @@ public class WebSecurityConfig {
             "/login", "/register",
             "/ads"
     };
-    @Autowired
-    DataSource dataSource;
-
-//    @Bean
-//    public AuthenticationManagerBuilder authenticationManagerBuilder(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.jdbcAuthentication()
-//                .dataSource(dataSource);
-//        return auth;
-//    }
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsService() {
-//        User.UserBuilder users = User.withDefaultPasswordEncoder();
-//        UserDetails user = users
-//                .username("user@gmail.com")
-//                .password("password")
-//                .roles("USER")
-//                .build();
-//        UserDetails admin = users
-//                .username("admin@gmail.com")
-//                .password("admin")
-//                .roles("ADMIN")
-//                .build();
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
+//    @Autowired
+//    DataSource dataSource;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .sessionManagement(session -> session.maximumSessions(1)//запрет на лог одного пользователя больше 1 раза
-                        .maxSessionsPreventsLogin(true)) //запрет второго логина
                 .csrf().disable()
                 .authorizeHttpRequests((authz) ->
                                 authz
