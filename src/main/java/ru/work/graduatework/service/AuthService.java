d83d3b6c97d589035c184f7d90ccb06fafd91a89
PDemenkov <guapnik@gmail.com>
2023-03-20 19:36:44 +0300

    Фикс секьюрити

--- src/main/java/ru/work/graduatework/service/AuthService.java
+++ src/main/java/ru/work/graduatework/service/AuthService.java
@@ -1,64 +1,61 @@
 package ru.work.graduatework.service;
 
+import lombok.RequiredArgsConstructor;
+import org.hibernate.ObjectNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import ru.work.graduatework.Entity.Users;
+import ru.work.graduatework.MySecurityConfig.UserDetailsServiceImpl;
 import ru.work.graduatework.dto.RegisterReqDto;
 import ru.work.graduatework.dto.Role;
+import ru.work.graduatework.mapper.UserMapper;
 import ru.work.graduatework.repository.UsersRepository;
 
-@Service()
+import javax.persistence.EntityNotFoundException;
+import javax.validation.ValidationException;
+import java.time.Instant;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
 
-    private final Logger logger = LoggerFactory.getLogger(AuthService.class);
-    private final UserDetailsManager manager;
-    private final UsersRepository usersRepository;
+
+    private final UserDetailsServiceImpl userDetailsService;
     private final PasswordEncoder encoder;
-
+    private final UsersRepository usersRepository;
+    private final UserMapper userMapper;
     private final UsersService usersService;
+    private final Logger logger = LoggerFactory.getLogger(AuthService.class);
 
-    public AuthService(UserDetailsManager manager, UsersRepository usersRepository, UsersService usersService) {
-        this.usersRepository = usersRepository;
-        this.manager = manager;
-        this.usersService = usersService;
-        this.encoder = new BCryptPasswordEncoder();
-    }
 
 
     public boolean login(String userName, String password) {
-        logger.info("Current method is - login");
-        if (!manager.userExists(userName)) {
-            return false;
-        }
-        UserDetails userDetails = manager.loadUserByUsername(userName);
-        String encryptedPassword = userDetails.getPassword();
-        String encryptedPasswordWithoutEncryptionType = encryptedPassword.substring(8);
-        return encoder.matches(password, encryptedPasswordWithoutEncryptionType);
-
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+            String encryptedPassword = userDetails.getPassword();
+            return encoder.matches(password, encryptedPassword);
     }
 
 
     public boolean register(RegisterReqDto registerReqDto, Role role) {
-        logger.info("Current method is - register");
-//        createUser(registerReqDto);   // method of adding a new user
-        if (manager.userExists(registerReqDto.getUsername())) {
-            return false;
-        }
-        manager.createUser(
-                User.withDefaultPasswordEncoder()
-                        .password(registerReqDto.getPassword())
-                        .username(registerReqDto.getUsername())
-                        .roles(role.name())
-                        .build()
-        );
-        return true;
+        Users user = userMapper.toEntity(registerReqDto);
+        if (usersRepository.existsByEmail(user.getEmail())) {
+            throw new EntityNotFoundException();}
+            user.setPassword(encoder.encode(user.getPassword()));
+            user.setRegDate(usersService.dateUserRegistration());
+            usersRepository.save(user);
+            return true;
     }
-
+}
     /**
      * @param registerReqDto Input parameter
      *                       <br> <b> Method create User </b> </br>
@@ -70,6 +67,3 @@ public class AuthService {
      */
 
 
-
-
-}
